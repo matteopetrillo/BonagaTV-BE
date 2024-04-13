f862a91e91ba8433c406a8603deeb25d6e322cee
Matteo Petrillo <commercial@matteopetrillo.info>
2024-04-13 18:07:52 +0200

    first commit

---
+++ src/main/java/it/petrillo/bonagatv/BonagatvBeApplication.java
@@ -0,0 +1,13 @@
+package it.petrillo.bonagatv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonagatvBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BonagatvBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/petrillo/bonagatv/BonagatvBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.petrillo.bonagatv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonagatvBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
