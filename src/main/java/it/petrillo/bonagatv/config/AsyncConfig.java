2d51badd6b5ab0b541239ec1753880ddb50701e1
Matteo Petrillo <111635558+matteopetrillo@users.noreply.github.com>
2025-11-06 16:56:50 +0100

    update async e prop

---
+++ src/main/java/it/petrillo/bonagatv/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package it.petrillo.bonagatv.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
